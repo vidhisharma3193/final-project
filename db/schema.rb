706b147848bf41b27fb886fc4d61cb50bf3a6cd7
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-10-02 16:14:34 -0500

    done

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170619203722) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
